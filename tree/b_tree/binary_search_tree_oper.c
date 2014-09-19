f85eeab71d20e715d1c0b1ab05d434a437c1fd6d
mankamana <mankamana.mishra@vssmonitoring.com>
2014-09-19 15:45:34 -0700

    addming some more new files

---
+++ tree/b_tree/binary_search_tree_oper.c
@@ -0,0 +1,31 @@
+ /******************************************************************************
+ * LEGALESE:   Copyright (c) 2014, Mankamana Prasad Mishra
+ * 
+ * This source code is confidential, proprietary, and contains trade$
+ * secrets that are the sole property of Mankamana Prasad Mishra
+ * Copy and/or distribution of this source code or disassembly or reverse$
+ * engineering of the resultant object code are strictly forbidden without$
+ * the written consent of Mankamana Prasad Mishra
+ * 
+ * *****************************************************************************
+ * 
+ * FILE NAME  :binary_tree_oper.c
+ * 
+ * DESCRIPTION: This file contains all the possible operation in binary 
+ * search tree.
+ *
+ * AUTHOR   : Mankamana Prasad Mishra
+ * 
+ *******************************************************************************/
+
+#include <binary_tree_oper.h>
+
+/*
+ * Main program, which would provide sample input and call the respective API to 
+ * make operation 
+ */
+int main (void)
+{
+    int input_array [] = {8, 3, 10, 1, 6, 14, 4, 7, 13};
+
+}
---
+++ tree/b_tree/binary_search_tree_oper.h
@@ -0,0 +1,27 @@
+ /******************************************************************************
+ * LEGALESE:   Copyright (c) 2014, Mankamana Prasad Mishra
+ * 
+ * This source code is confidential, proprietary, and contains trade$
+ * secrets that are the sole property of Mankamana Prasad Mishra
+ * Copy and/or distribution of this source code or disassembly or reverse$
+ * engineering of the resultant object code are strictly forbidden without$
+ * the written consent of Mankamana Prasad Mishra
+ * 
+ * *****************************************************************************
+ * 
+ * FILE NAME  :binary_tree_oper.h
+ * 
+ * DESCRIPTION: This file contains all the declaration needed for binary tree
+ *
+ * AUTHOR   : Mankamana Prasad Mishra
+ * 
+ *******************************************************************************/
+
+#include <stdio.h>
+
+typedef struct bst_type {
+    int data;            // Data to be stored in node
+    bst_type *left;      // pointer to left node
+    bst_type *right;     // pointer to right node
+    bst_type *parent;    // pointer to parent node 
+}bst_node;
