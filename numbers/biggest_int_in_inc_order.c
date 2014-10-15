3612d3e441a9a081a69b7bfcfbf3aac806e427cf
mankamana <mankamana.mishra@vssmonitoring.com>
2014-10-15 12:56:30 -0700

    http://www.geeksforgeeks.org/amazon-interview-experience-set-133/

---
+++ numbers/biggest_int_in_inc_order.c
@@ -0,0 +1,17 @@
+/*Given an integer, find the next biggest integer whose digits are in increasing order.
+ *
+ * Example:
+ * Input: 118
+ * Output: 123
+ *
+ * Input: 127
+ * Output: 234
+ *
+ * Input: 987
+ * Output: 1234
+ */
+
+int biggest_number (int num_of_digit, int first_digit, int number)
+{
+
+}
