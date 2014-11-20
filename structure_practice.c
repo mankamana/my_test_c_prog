995048ace7ef2c12ca989cca11699d11dcbec51e
Mankamana prasad Mishra <pragati@Mankamanas-MacBook-Air.local>
2014-11-20 09:28:06 -0800

    structure practice

---
+++ structure_practice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct human_t_ {
+    char *name;
+    int age;
+} ;
+
+typedef struct animal_t_ {
+    char *type;
+    int age;
+} animal_t ;
+
+
+void main ()
+{
+    
+}
